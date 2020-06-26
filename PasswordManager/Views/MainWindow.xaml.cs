58415cf07afe122b1ac985169217abe4f8b39fc1
Eloi Rivière <eloi.riviere@hotmail.com>
2020-06-26 16:46:06 +0200

    ajout modification, annulation, résolution des bugs, ajouts, suppression, modification en base

--- PasswordManager/Views/MainWindow.xaml.cs
+++ PasswordManager/Views/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace PasswordsManager.Views
         private MainViewModel mvm;
         private Boolean isEditionOpened;
         private List<Password> listePasswords = new List<Password>();
+        private static Boolean isModification = false;
+        private static int EditionColumnSizeBase = 2;
 
         public MainWindow()
         {
@@ -28,36 +30,11 @@ namespace PasswordsManager.Views
             DataContext = mvm;
 
             isEditionOpened = false;
-
-            /*
-            var t = new Models.Tag()
-            {
-                Label = "tag"
-            };
-
-            var p = new Models.Password()
-            {
-                Label = "label1",
-                Login = "login1",
-                Pass = "pass1",
-                Url = "url1"
-            };
-
-            p.Tags = new List<PasswordTag>();
-            p.Tags.Add(new PasswordTag()
-            {
-                Tag = t
-            });
-
-            mvm.listeSauvegarde = new System.Collections.ObjectModel.ObservableCollection<Password>();
-            mvm.listeSauvegarde.Add(p);
-            */
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Volet(object sender, RoutedEventArgs e)
         {
             int EditionColumnSize = 2;
-
 
             if (isEditionOpened)
             {
@@ -121,6 +98,7 @@ namespace PasswordsManager.Views
         {
             Console.WriteLine("Evènement - ListBox - SelectionChanged");
             ButtonSupprimer.Visibility = Visibility.Visible;
+            ButtonModifier.Visibility = Visibility.Visible;
 
         }
 
@@ -128,47 +106,142 @@ namespace PasswordsManager.Views
         {
             Console.WriteLine("Evènement - Formulaire - Validation : " + Recherche.Text);
 
-            string encryptedPassword = Crypto.Encrypt(mvm.FormulaireMotDePasse);
-
-            Password password = new Models.Password()
+            if (mvm.FormulaireLabel.Equals(String.Empty)
+               || mvm.FormulaireMotDePasse.Equals(String.Empty)
+               || mvm.FormulaireURL.Equals(String.Empty))
             {
-                Label = mvm.FormulaireLabel,
-                Login = mvm.FormulaireIdentifiant,
-                Pass = mvm.FormulaireMotDePasse,
-                Url = mvm.FormulaireURL
-            };
-
-            Tag tag = new Models.Tag()
+                Console.WriteLine("Manque de données dans le formulaire : Label, Mot de passe ou Url.");
+            }
+            else
             {
-                Label = mvm.FormulaireTags
-            };
 
-            password.Tags = new List<PasswordTag>();
+                string encryptedPassword = Crypto.Encrypt(mvm.FormulaireMotDePasse);
 
-            PasswordTag passwordTag = new PasswordTag()
-            {
-                Tag = tag
-            };
+                Password password = new Models.Password()
+                {
+                    Label = mvm.FormulaireLabel,
+                    Login = mvm.FormulaireIdentifiant,
+                    Pass = mvm.FormulaireMotDePasse,
+                    Url = mvm.FormulaireURL
+                };
 
-            DataAccess.PasswordsDbContext.Current.Add(password);
+                Tag tag = new Models.Tag()
+                {
+                    Label = mvm.FormulaireTags
+                };
 
-            Console.WriteLine("Password créé : " + password);
+                password.Tags = new List<PasswordTag>();
 
-            mvm.listeSauvegarde.Add(password);
-            Console.WriteLine(password);
+                PasswordTag passwordTag = new PasswordTag()
+                {
+                    Tag = tag
+                };
 
-            // cryptage du mot de passe et mise en base
-            password.Pass = encryptedPassword;
-            DataAccess.PasswordsDbContext.Current.Add(password);
+                if (isModification)
+                {
+                    Models.Password selectedPassword = (Models.Password)Liste_Passwords.SelectedItem;
+                    mvm.listeSauvegarde.Remove(selectedPassword);
+                    mvm.listeSauvegarde.Add(password);
+
+                    DataAccess.PasswordsDbContext.Current.Remove(selectedPassword);
+                    DataAccess.PasswordsDbContext.Current.Add(password);
+                    try
+                    {
+                        DataAccess.PasswordsDbContext.Current.SaveChanges();
+                    }
+                    catch (Microsoft.EntityFrameworkCore.DbUpdateException dbue) { };
+                    Vider_Formulaire();
+                    isModification = false;
+                }
+                else
+                {
+                    DataAccess.PasswordsDbContext.Current.Add(password);
+
+                    Console.WriteLine("Password créé : " + password);
+
+                    mvm.listeSauvegarde.Add(password);
+                    Console.WriteLine(password);
+
+                    // cryptage du mot de passe et mise en base
+                    password.Pass = encryptedPassword;
+                    DataAccess.PasswordsDbContext.Current.Add(password);
+                }
+            }
         }
 
         private void Button_Supprimer(object sender, RoutedEventArgs e)
         {
+            if(isModification)
+            {
+                isModification = false;
+                Edition.Width = new GridLength(0, GridUnitType.Star);
+                Vider_Formulaire();
+
+            }
             Models.Password password = (Models.Password)Liste_Passwords.SelectedItem;
             DataAccess.PasswordsDbContext.Current.Remove(password);
-            DataAccess.PasswordsDbContext.Current.SaveChanges();
+            try
+            {
+                DataAccess.PasswordsDbContext.Current.SaveChanges();
+            }
+            catch (Microsoft.EntityFrameworkCore.DbUpdateException dbue) { };
             mvm.listeSauvegarde.Remove(password);
+
+            if(isModification)
+            {
+                Vider_Formulaire();
+            }
+
             ButtonSupprimer.Visibility = Visibility.Collapsed;
+            ButtonModifier.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Modifier(object sender, RoutedEventArgs e)
+        {
+            isModification = true;
+            Models.Password password = (Models.Password)Liste_Passwords.SelectedItem;
+
+            mvm.FormulaireLabel = password.Label;
+            mvm.FormulaireIdentifiant = password.Login;
+            // mvm.FormulaireMotDePasse = String.Empty;
+            mvm.FormulaireMotDePasse = password.Pass;
+            mvm.FormulaireURL = password.Url;
+
+            String tags = String.Empty;
+
+            foreach(PasswordTag pt in password.Tags)
+            {
+                tags += ", " + pt;
+            }
+
+            mvm.FormulaireTags = tags;
+
+            Edition.Width = new GridLength(EditionColumnSizeBase, GridUnitType.Star);
+            isEditionOpened = true;
+
+            ButtonModifier.Visibility = Visibility.Collapsed;
+        }
+
+        private void Vider_Formulaire()
+        {
+            mvm.FormulaireLabel = String.Empty;
+            mvm.FormulaireIdentifiant = String.Empty;
+            mvm.FormulaireMotDePasse = String.Empty;
+            mvm.FormulaireURL = String.Empty;
+            mvm.FormulaireURL = String.Empty;
+        }
+
+        private void Formulaire_Button_Annuler_Click(object sender, RoutedEventArgs e)
+        {
+            if(isModification)
+            {
+                isModification = false;
+            }
+            Vider_Formulaire();
+            ButtonModifier.Visibility = Visibility.Collapsed;
+            ButtonSupprimer.Visibility = Visibility.Collapsed;
+            Edition.Width = new GridLength(0, GridUnitType.Star);
+            isEditionOpened = false;
         }
     }
 }
